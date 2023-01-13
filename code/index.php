a05f1bb07990fbfcce39b3c948acbc771d55b338
Soufian Tamim <soufiaintamim22@gmail.com>
2023-01-13 16:43:51 +0100

    initial commit

---
+++ code/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css"/>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="shortcut icon" href="#" />
+  <title>Document</title>
+</head>
+<body>
+    <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Teams</th>
+        <th scope="col">pts</th>
+        <th scope="col">G.P</th>
+        <th scope="col">G.W</th>
+        <th scope="col">G.E</th>
+        <th scope="col">G.L</th>
+        <th scope="col">GO.S</th>
+        <th scope="col">GO.R</th>
+        <th scope="col">+/-</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Marooco</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Spain</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Portugual</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Iran</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <?php
+
+
+  ?>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
