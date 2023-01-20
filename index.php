8cc67a3da4f43e2607605ddf3f3b1bdcfe9a076d
soufian Tamim <soufiantamim22@gamil.com>
2023-01-20 16:10:42 +0100

    initial commit

--- index.php
+++ index.php
@@ -1,22 +1,232 @@
 <?php
-require 'function.php';
+//=========================================================================
+//========================= functions =====================================
+//=========================================================================
+
+function Geter( $array , $TeamOne ){
+    $DataArr = $array ;
+     unset($DataArr[$TeamOne]);
+     unset($DataArr["STAT"]);
+foreach ($DataArr as $key => $value) {
+    return $key ;
+}
+}
+function simulate($Results){
+    $teams = [];
+    $TeamKeys = array();
+    foreach($Results as $key => $val) {
+        $contries = array();
+        $values = array();
+     foreach($val as $valkey => $miniVAL){
+         array_push($contries, $valkey);
+         array_push($values, $miniVAL);
+         array_push($TeamKeys, $valkey);
+     }
+    }
+    foreach ($TeamKeys as $key => $value) {
+        if($value == "STAT"){
+            unset($TeamKeys[$key]);
+        }
+    }
+    $TeamKeys = array_values(array_unique($TeamKeys));
+    foreach($TeamKeys as $value){
+     $teams += [$value => array("points" => 0 , "games_played" => 0 , "games_won" => 0  , "games_equal" => 0  , "games_lose" => 0  , "goals_scored" => 0  , "goals_received" => 0  , "difference" => 0 )];
+    }
+    foreach ($teams as $key => $value) {
+        $games_played = 0 ;
+        $games_won = 0 ;
+        $games_equal = 0 ;
+        $games_lose = 0 ;
+        $points = ( $games_won * 3 ) + ( $games_equal * 1 ) ;
+        $goals_scored = 0 ;
+        $goals_received = 0 ;
+        $difference = $goals_scored - $goals_received ;
+
+        foreach ($Results as $Dkey => $Dvalue) {
+            if(isset($Dvalue[$key])){
+                $goals_scored += $Dvalue[$key] ;
+                $goals_received += $Dvalue[Geter( $Dvalue , $key )] ;
+                if($Dvalue["STAT"] == true){
+                    $games_played += 1 ;
+                }
+    if( $Dvalue[$key] > $Dvalue[Geter( $Dvalue , $key )] ){
+        $games_won += 1;
+    } elseif( $Dvalue[$key] < $Dvalue[Geter( $Dvalue , $key )] ){
+        $games_lose += 1;
+    } elseif( $Dvalue[$key] == $Dvalue[Geter( $Dvalue , $key )] ){
+        $games_equal += 1;
+    } 
+            }
+        }
+        $teams[$key]["goals_scored"] = $goals_scored ; 
+        $teams[$key]["goals_received"] = $goals_received ; 
+        $teams[$key]["difference"] = $goals_scored - $goals_received ; 
+        $teams[$key]["games_played"] = $games_played ; 
+        $teams[$key]["games_won"] = $games_won ; 
+        $teams[$key]["games_lose"] = $games_lose ; 
+        $teams[$key]["games_equal"] = $games_equal ; 
+        $teams[$key]["points"] = ( $games_won * 3 ) + ( $games_equal * 1 ) ; 
+    }
+    return Reseter($teams);
+    };
+    function Reseter($data) {
+        foreach($data as $key => $value) {
+            foreach ($value as $xkey => $xvalue) {
+                $data[$key]["team"] = $key ;
+            }
+        }
+        $FormArr = [];
+        foreach($data as $key => $value) {
+          array_push($FormArr , $value );
+        }
+        return $FormArr;
+    }
+function SortTwo($data){
+    global $matches;
+    usort($data, function ($x, $y) {
+        global $matches;
+        if ($x["points"] === $y["points"]) {
+            if ($x["difference"] === $y["difference"]) {
+                if ($x["goals_scored"] === $y["goals_scored"]) {
+                    foreach ( $matches as $Mkey => $Mvalue) {
+                        if(isset($Mvalue[$x["team"]])  && isset( $Mvalue[$y["team"]] )){
+                            if ( $Mvalue[$x["team"]] === $Mvalue[$y["team"]]) {
+                        return 0;
+                    } else if ( $Mvalue[$x["team"]] < $Mvalue[$y["team"]] ) {
+                        return 1 ;
+                    } else if ( $Mvalue[$x["team"]] > $Mvalue[$y["team"]] ) {
+                        return -1 ;
+                    }
+                }
+                }
+                } else if ( $x["goals_scored"] < $y["goals_scored"] ) {
+                    return 1 ;
+                } else if ( $x["goals_scored"] > $y["goals_scored"] ) {
+                    return -1 ;
+                }
+            } else if ( $x["difference"] < $y["difference"] ) {
+                return 1 ;
+            } else if ( $x["difference"] > $y["difference"] ) {
+                return -1 ;
+            }
+        } else if ( $x["points"] < $y["points"] ) {
+            return 1 ;
+        } else if ( $x["points"] > $y["points"] ) {
+            return -1 ;
+        }
+    });
+    return $data;
+}
+function GETnotTWOteams($data){
+    usort($data, function ($x, $y) {
+        if ($x["points"] === $y["points"]) {
+            if ($x["difference"] === $y["difference"]) {
+                if ($x["goals_scored"] === $y["goals_scored"]) {
+                        return 0;
+                } else if ( $x["goals_scored"] < $y["goals_scored"] ) {
+                    return 1 ;
+                } else if ( $x["goals_scored"] > $y["goals_scored"] ) {
+                    return -1 ;
+                }
+            } else if ( $x["difference"] < $y["difference"] ) {
+                return 1 ;
+            } else if ( $x["difference"] > $y["difference"] ) {
+                return -1 ;
+            }
+        } else if ( $x["points"] < $y["points"] ) {
+            return 1 ;
+        } else if ( $x["points"] > $y["points"] ) {
+            return -1 ;
+        }
+    });
+    return $data;
+}
+function GetEqual($data){
+    $Common = array() ;
+    $catch = array();
+    foreach ($data as $key => $value) {
+        $Usual = array() ;
+        $i = 1;
+        foreach ($data as $xkey => $xvalue) {
+            if($value["points"] === $xvalue["points"] &&  $value["difference"] === $xvalue["difference"] &&  $value["goals_scored"] === $xvalue["goals_scored"] && $value["team"] != $xvalue["team"] && !in_array($xvalue["team"], $catch)  && !in_array($value["team"], $catch)  ){
+                if ($i === 1 ){
+                    array_push( $Usual ,  $value );
+                    $i++;
+                }
+                array_push( $Usual ,  $xvalue );
+                array_push( $catch ,  $xvalue["team"] );
+                   }
+                }
+                if( ! count($Usual) == 0){
+                    array_push( $Common ,  $Usual );
+                }
+    }
+     return $Common ;
+}
+function GetDiff($array){
+    $TeamsCountries = array("MOROCCO","IRAN","PORTUGUAL","SPAIN");
+        $AvTeams = array();
+        foreach ( $array as $key => $value) {
+            array_push( $AvTeams , $value["team"]); 
+        }
+        $differenceValues = array_values(array_difference($TeamsCountries,$AvTeams));
+        return $differenceValues;
+}
+function getWitoutteams($NewItems , $MArray  ){
+    $TheMArray = $MArray ;
+    foreach($TheMArray as $ykey => $yval) {
+        foreach($yval as $gkey => $gval) {
+    foreach ( $NewItems  as $xkey => $xvalue) {
+    if( $gkey == $xvalue ){
+            unset($TheMArray[$ykey]);
+        }
+    }
+    }
+    }
+    return $TheMArray;
+}
+function SortArr($AllArr , $PoV){
+$data = $AllArr ;
+        $PoArr = array();
+        foreach ($PoV as $Pkey => $Pvalue) {
+          array_push($PoArr ,  $Pvalue["team"]  ) ; 
+        }
+        for ($i=0; $i < count($data) ; $i++) { 
+            if( in_array( $data[$i]["team"] , $PoArr  ) ) {
+                $data[$i] = $PoV[0];
+                $data[$i+1] = $PoV[1];
+                $data[$i+2] = $PoV[2];
+                break;
+            } 
+        }
+        foreach ( $data as $key => $value) {
+            foreach ( $AllArr as $xkey => $xvalue) {
+            if($value["team"] == $xvalue["team"]  )
+            $data[$key] = $xvalue ;
+            }
+        }
+        return $data ;
+}
+//=========================================================================
+//========================= check the data ================================
+//=========================================================================
 if(isset($_COOKIE["matches"])){
     $matches = json_decode($_COOKIE['matches'] , true );
 } else {
 $matches = ["MOROCCO_VS_IRAN" =>["MOROCCO" => 0,"IRAN" => 0,"STAT" => false], 
-            "POTUGUAL_VS_SPAIN" =>    ["PORTUGUAL" => 0,"SPAIN" => 0,"STAT" => false],
-            "MOROCCO_VS_SPAIN" =>     ["MOROCCO" => 0,"SPAIN" => 0,"STAT" => false],
-            "PORTUGUAL_VS_IRAN" =>    ["PORTUGUAL" => 0,"IRAN" => 0,"STAT" => false],
-            "MOROCCO_VS_PORTUGUAL" => ["MOROCCO" => 0,"PORTUGUAL" => 0,"STAT" => false],
-            "IRAN_VS_SPAIN" =>        ["IRAN" => 0, "SPAIN" => 0,"STAT" => false]
-          ]; 
+"POTUGUAL_VS_SPAIN" =>    ["PORTUGUAL" => 0,"SPAIN" => 0,"STAT" => false],
+"MOROCCO_VS_SPAIN" =>     ["MOROCCO" => 0,"SPAIN" => 0,"STAT" => false],
+"PORTUGUAL_VS_IRAN" =>    ["PORTUGUAL" => 0,"IRAN" => 0,"STAT" => false],
+"MOROCCO_VS_PORTUGUAL" => ["MOROCCO" => 0,"PORTUGUAL" => 0,"STAT" => false],
+"IRAN_VS_SPAIN" =>        ["IRAN" => 0, "SPAIN" => 0,"STAT" => false]
+]; 
 }
 $Table = [
-["team"=> "MOROCCO" ,"points" => $ptsM = 0,"games_played" => $playedM = 0,"games_won" => $wonM = 0,"games_equal" => $equalM = 0,"games_lose" => $loseM = 0, "goals_scored" => $scoredM = 0,"goals_received" => $receivedM = 0 , "difference" => $differenceM = 0],
-["team"=> "IRAN" ,"points" => $ptsI = 0,"games_played" => $playedI = 0, "games_won" => $wonI = 0,"games_equal" => $equalI = 0, "games_lose" => $loseI = 0, "goals_scored" => $scoredI = 0, "goals_received" => $receivedI = 0, "difference" => $differenceI = 0],
-["team"=> "PORTUGUAL" ,"points" => $ptsP = 0,"games_played" => $playedP = 0, "games_won" => $wonP = 0,"games_equal" => $equalP = 0, "games_lose" => $loseP = 0, "goals_scored" => $scoredP = 0, "goals_received" => $receivedP = 0, "difference" => $differenceP = 0],
-["team"=> "SPAIN" ,"points" => $ptsS = 0,"games_played" => $playedS = 0, "games_won" => $wonS = 0,"games_equal" => $equalS = 0, "games_lose" => $loseS = 0, "goals_scored" => $scoredS = 0, "goals_received" => $receivedS = 0, "difference" => $differenceS = 0]
-]; 
+    ["team"=> "MOROCCO" ,"points" => 0,"games_played" => 0,"games_won" => 0,"games_equal" => 0,"games_lose" => 0, "goals_scored" => 0,"goals_received" => 0 , "difference" => 0],
+    ["team"=> "IRAN" ,"points" => 0,"games_played" => 0, "games_won" => 0,"games_equal" => 0, "games_lose" => 0, "goals_scored" => 0, "goals_received" => 0, "difference" => 0],
+    ["team"=> "PORTUGUAL" ,"points" => 0,"games_played" => 0, "games_won" => 0,"games_equal" => 0, "games_lose" => 0, "goals_scored" => 0, "goals_received" => 0, "difference" => 0],
+    ["team"=> "SPAIN" ,"points" => 0,"games_played" => 0, "games_won" => 0,"games_equal" => 0, "games_lose" => 0, "goals_scored" => 0, "goals_received" => 0, "difference" => 0]
+  ]; 
 
 if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['MatchName']) && isset($_POST['TeamOne'])  && isset($_POST['TeamTwo']) ){
     $matches[$_POST['MatchName']][$_POST['TeamOne'][0]] = $_POST['TeamOne'][1] ;
@@ -25,12 +235,12 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['MatchName']) && isset(
     setcookie('matches', json_encode($matches));
   } elseif($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['reset'])){
     $matches = ["MOROCCO_VS_IRAN" =>["MOROCCO" => 0,"IRAN" => 0,"STAT" => false], 
-                "POTUGUAL_VS_SPAIN" =>    ["PORTUGUAL" => 0,"SPAIN" => 0,"STAT" => false],
-                "MOROCCO_VS_SPAIN" =>     ["MOROCCO" => 0,"SPAIN" => 0,"STAT" => false],
-                "PORTUGUAL_VS_IRAN" =>    ["PORTUGUAL" => 0,"IRAN" => 0,"STAT" => false],
-                "MOROCCO_VS_PORTUGUAL" => ["MOROCCO" => 0,"PORTUGUAL" => 0,"STAT" => false],
-                "IRAN_VS_SPAIN" =>        ["IRAN" => 0, "SPAIN" => 0,"STAT" => false]
-              ]; 
+      "POTUGUAL_VS_SPAIN" =>    ["PORTUGUAL" => 0,"SPAIN" => 0,"STAT" => false],
+      "MOROCCO_VS_SPAIN" =>     ["MOROCCO" => 0,"SPAIN" => 0,"STAT" => false],
+      "PORTUGUAL_VS_IRAN" =>    ["PORTUGUAL" => 0,"IRAN" => 0,"STAT" => false],
+      "MOROCCO_VS_PORTUGUAL" => ["MOROCCO" => 0,"PORTUGUAL" => 0,"STAT" => false],
+      "IRAN_VS_SPAIN" =>        ["IRAN" => 0, "SPAIN" => 0,"STAT" => false]
+    ]; 
     setcookie('matches', json_encode($matches));
   }
   if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['MatchName']) && isset($_POST['TeamOne'])  && isset($_POST['TeamTwo']) ){
@@ -47,7 +257,10 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['MatchName']) && isset(
                  ]; 
   }
 ?>
-
+<!--  =========================================================================
+      ========================= body of table and forms =======================
+      ========================================================================= 
+-->
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -59,6 +272,11 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['MatchName']) && isset(
     <title>WORLD CUP</title>
   </head>
   <body>
+    <!-- 
+    =========================================================================
+    =========================  forms build ==================================
+    ========================================================================= 
+    -->
     <h1 class="text-center text-primary">WORLD CUP SIMULATOR</h1>
         <h2 class="text-warning text-center">GROUP F MATCHES</h2>
             <div class="text-center" >
@@ -100,6 +318,13 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['MatchName']) && isset(
           <input type="hidden" name="_method" value="PUT">
           <input type="submit"  class="btn btn-success w-25 container"  value="SIMULATE">
       </form>
+
+
+    <!-- 
+    =========================================================================
+    ========================= table build ===================================
+    =========================================================================
+    -->
       <table class="table table-striped table-bordered  h-25 mt-5">
         <thead>
           <tr>
@@ -117,6 +342,7 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['MatchName']) && isset(
         </thead>
         <tbody>
           <?php
+          //=========== function that builds table empty ==============
             function buildTable(){
               $count = 0;
               foreach($GLOBALS['Table'] as $xkey =>
@@ -127,10 +353,11 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['MatchName']) && isset(
                   } 
                   echo "</tr>"; 
                 }   
-            }        
+            }    
+          //=========== sort the teams by points ==============
               if( isset($_REQUEST['_method']) &&  $_REQUEST['_method'] == "PUT") {
-              if(count(GETCOMMONS(resultCouter($matches))) === 0 ){
-                  foreach (  sortByTwoEquals(resultCouter($matches))  as $key => $value) {
+              if(count(GetEqual(simulate($matches))) === 0 ){
+                  foreach (SortTwo(simulate($matches))  as $key => $value) {
                       ?>
                       <tr>
                       <td> <?php echo $key + 1 ; ?> </td>
@@ -146,10 +373,11 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['MatchName']) && isset(
                     </tr>
                     <?php 
                   }
+               //===========    ==============
               } else {
-                  if(count(GETCOMMONS(resultCouter($matches))) == 1){
-                      if(count(GETCOMMONS(resultCouter($matches))[0]) == 2){
-                      foreach ( sortByTwoEquals(resultCouter($matches) )  as $key => $value) {
+                  if(count(GetEqual(simulate($matches))) == 1){
+                      if(count(GetEqual(simulate($matches))[0]) == 2){
+                      foreach (SortTwo(simulate($matches) )  as $key => $value) {
                           ?>
                           <tr>
                           <td> <?php echo $key + 1 ; ?> </td>
@@ -165,9 +393,9 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['MatchName']) && isset(
                         </tr>
                         <?php 
                       }
-                  } elseif ( count(GETCOMMONS(resultCouter($matches))[0]) > 2) {
-                    $theCommonItems =  GETCOMMONS(resultCouter($matches))[0];
-                    $rightsortedData = ArrayRightSort( SortWithoutComparingtwoTeams(resultCouter($matches)) , sortByTwoEquals(resultCouter(getTheMAtchesResultsWithoutTheNotConcernedTeams( GetTheDiffTeams($theCommonItems) , $matches )))) ;
+                  } elseif ( count(GetEqual(simulate($matches))[0]) > 2) {
+                    $theCommonItems =  GetEqual(simulate($matches))[0];
+                    $rightsortedData = SortArr( GETnotTWOteams(simulate($matches)) , SortTwo(simulate(getWitoutteams( GetTheDiffTeams($theCommonItems) , $matches )))) ;
                       foreach ( $rightsortedData   as $key => $value) {
                           ?>
                           <tr>
@@ -185,8 +413,8 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['MatchName']) && isset(
                         <?php 
                       }
                 }
-                  } elseif ( count(GETCOMMONS(resultCouter($matches)))  == 2) {
-                      foreach ( sortByTwoEquals(resultCouter($matches))  as $key => $value) {
+                  } elseif ( count(GetEqual(simulate($matches)))  == 2) {
+                      foreach ( SortTwo(simulate($matches))  as $key => $value) {
                           ?>
                           <tr>
                           <td><?php  echo $key + 1 ; ?> </td>
